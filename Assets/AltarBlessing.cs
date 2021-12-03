38260bd82e4fa46e3870b3ad31b37262e94cd99c
weirdcloud <63076259+weirdcloud@users.noreply.github.com>
2021-12-03 20:08:04 +0200

    Added basic altar functionality

---
+++ Assets/AltarBlessing.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AltarBlessing : MonoBehaviour
+{
+    [SerializeField]
+    PowerUps activePowerUps;
+    [SerializeField]
+    CurrencyCount currencyCount;
+    [SerializeField]
+    int price = 10;
+
+    public void buyPowerUp()
+    {
+        if (currencyCount.soulsAmount >= price)
+        {
+            currencyCount.soulsAmount -= price;
+            enableRandomPowerUp();
+        }
+    }
+
+    void enableRandomPowerUp()
+    {
+        int random = Random.Range(0, 2);
+        switch(random)
+        {
+            case 1:
+                activePowerUps.doubleDamage = true;
+                break;
+            case 2:
+                activePowerUps.quickRecharge = true;
+                break;
+            case 3:
+                activePowerUps.tripleJump = true;
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/PowerUps.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PowerUps : ScriptableObject
+{
+    public bool doubleDamage;
+    public bool quickRecharge;
+    public bool tripleJump;
+}
